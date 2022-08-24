52d0ba6c3367e6c55baa467400385eecbb13ace9
PrasadKurale <kuralers@gmail.com>
2022-08-24 13:49:43 +0530

    DATA UPDATE SUCCESSFULLY

--- Cust_Mgnt_JPA/src/main/java/com/csi/controller/CustomerController.java
+++ Cust_Mgnt_JPA/src/main/java/com/csi/controller/CustomerController.java
@@ -60,9 +60,13 @@ public class CustomerController {
     {
         return "HII SPIDERMAN";
     }
-    @GetMapping
-    public String sayService("/service")
+    @GetMapping("/service")
+    public String sayService()
     {
         return "Service is good";
+    }
+    @GetMapping("/hello")
+    public  String hello(){
+        return "Welcome to csi";
     }
 }
